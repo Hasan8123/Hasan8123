40851bc1f53d10878326da76c86616e59adf6520
Hasan Sheikh <139635509+Hasan8123@users.noreply.github.com>
2023-07-21 01:08:02 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,70 @@
+console.log("hello");
+console.log("how was the day");
+console.log("It could be better");
+
+const num:number=18; 
+console.log(num)
+
+
+const fname1 = "Ali";
+const age1 = "22";
+const message1 = `hello ${fname1}, my age is ${age1}`;
+console.log (message1);
+
+
+var fname2 = "ALi";
+var sting2 = "Hello";
+var message2 = fname2 + sting2;
+console.log (message2);
+
+
+var fname3 = "Ali";
+var string3 = "Hello" + " " + fname3;
+console.log (string3);
+
+
+let firstname = "Ali"; 
+let age = 21;
+let message = `Hello my name is ${firstname}, I my ${age} year old`;
+console.warn (message);
+
+
+console.log ("Arithmetic Operator"); 
+console.warn ("Addition");
+let n1 = 2;
+let n2 = 8;
+console.log (n1 + n2 );
+
+let string1 = "4";
+let string2 = "6";
+console.log (string1 + string2);
+
+
+console.log ("Subtract")
+let g1= 7;
+let g2 = 5;
+console.log (g1 - g2);
+
+
+console.log ("Multi")
+let d3= 5;
+let d7= 2;
+console.log (d3 * d7)
+
+
+console.log ("Div")
+let n4= 15;
+let n5= 3;
+console.log (n4/n5);
+
+
+console.log ("Exp");
+let j6= 2;
+let k7= 4;
+console.log (j6 ** k7);
+
+
+console.log ("Modulus")
+let u7= 10;
+let u8= 3;
+console.log (u7 % u8)
